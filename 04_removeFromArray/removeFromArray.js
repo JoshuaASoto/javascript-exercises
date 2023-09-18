d8c3485e6148fdecc2e3196a8bb5cb59e736aa68
JoshuaASoto <joshua.antonio.soto@gmail.com>
2023-09-18 18:50:27 -0500

    completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...x) {
+    const newArray = [];
+    array.forEach((item) => {
+        if (!x.includes(item)) {
+        newArray.push(item);
+        }
+    });
+    return newArray
 };
 
 // Do not edit below this line
