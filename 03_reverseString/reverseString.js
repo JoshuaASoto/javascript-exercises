5d7d3d38c2864baa00090626af655fd6ffac6035
JoshuaASoto <joshua.antonio.soto@gmail.com>
2023-09-18 14:40:04 -0500

    update

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let string ="";
+    for (var i = str.length - 1; i>=0; i--) {
+        string += str[i];
+    }
+    return string;
 };
 
 // Do not edit below this line
